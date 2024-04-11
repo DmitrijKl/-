44ce350091bce51557106babd099aa29d069df8e
DmitrijKl <klyanchenkov.dmitrij@.mail.ru>
2024-04-11 19:23:12 +0400

    Added Form html

--- index.js
+++ index.js
@@ -136,6 +136,15 @@ function init() {
   }
   showCategory(activeCategory);
 
+  const zoomPlus = document.querySelector(".zoom__plus");
+  const zoomMinus = document.querySelector(".zoom__minus");
+  zoomPlus.addEventListener("click", function () {
+    map.setZoom(map.getZoom() + 1);
+  });
+  zoomMinus.addEventListener("click", function () {
+    map.setZoom(map.getZoom() - 1);
+  });
+
   yrFace.addEventListener("click", function () {
     activeCategory = "ЮрЛицо";
     showCategory(activeCategory);
@@ -197,4 +206,33 @@ btnCloseModal.addEventListener("click", (event) => {
 });
 btnOpenModal.addEventListener("click", (event) => {
   divModalWindow.classList.toggle("active");
+});
+
+const popupTitleActive = document.querySelector(".popup__title");
+const popupHeader = document.querySelector(".popup__header");
+const contentTitle = document.querySelector(".content__title");
+const popup = document.querySelector(".popup");
+
+const headerHeight = popupHeader.offsetTop;
+const contentTitleHeight = contentTitle.offsetTop;
+
+popup.addEventListener("scroll", () => {
+  const scrollPopup = popup.scrollTop;
+  console.log(scrollPopup);
+  console.log(contentTitleHeight, "title");
+  if (scrollPopup > contentTitleHeight) {
+    popupTitleActive.classList.add("active");
+    popupHeader.classList.add("active");
+  } else {
+    popupTitleActive.classList.remove("active");
+    popupHeader.classList.remove("active");
+  }
+});
+
+const popupBody = document.querySelector(".popup__body");
+
+popupBody.addEventListener("click", (event) => {
+  if (event.currentTarget === event.target) {
+    divModalWindow.classList.toggle("active");
+  }
 });
