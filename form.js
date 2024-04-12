6aed13972765f7a59a574edd79b11bed61d9592a
DmitrijKl <klyanchenkov.dmitrij@.mail.ru>
2024-04-12 11:04:08 +0400

    Added features for form

--- form.js
+++ form.js
@@ -36,3 +36,50 @@ popupBody.addEventListener("click", (event) => {
     divModalWindow.classList.toggle("active");
   }
 });
+
+const checkbox = document.getElementById("checkbox");
+const labelCheckbox = document.querySelector(".checkbox__label");
+const checkboxConfirm = document.getElementById("checkboxConfirm");
+const labelCheckboxConfirm = document.querySelector(".label__confirm");
+
+checkbox.addEventListener("change", () => {
+  labelCheckbox.classList.toggle("active");
+});
+checkboxConfirm.addEventListener("change", () => {
+  labelCheckboxConfirm.classList.toggle("active");
+});
+
+document.getElementById("tel").onkeydown = function (e) {
+  inputphone(e, document.getElementById("tel"));
+};
+
+function inputphone(e, phone) {
+  function stop(evt) {
+    evt.preventDefault();
+  }
+  let key = e.key,
+    v = phone.value;
+  not = key.replace(/([0-9])/, 1);
+
+  if (not == 1 || "Backspace" === not) {
+    if ("Backspace" != not) {
+      if (v.length < 3 || v === "") {
+        phone.value = "+7(";
+      }
+      if (v.length === 6) {
+        phone.value = v + ")";
+      }
+      if (v.length === 10) {
+        phone.value = v + "-";
+      }
+      if (v.length === 13) {
+        phone.value = v + "-";
+      }
+      if (v.length === 16) {
+        stop(e);
+      }
+    }
+  } else {
+    stop(e);
+  }
+}
